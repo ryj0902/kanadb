99b828c9a05218e723fd4cd802d439fb80bd260c
ryj0902 <ryj0902@naver.com>
2024-01-01 13:37:59 +0900

    * add link card * refactoring some js function

--- static/card/select_card.js
+++ static/card/select_card.js
@@ -1,18 +1,37 @@
-function drawCard(card, top_class){
+function drawCard(card, className){
     result = '' +
-    '<div class="' + top_class  + (card.producible ? '' : ' unproducible-card') + '">' +
+    '<div class="' + className + '-image'  + (card.producible ? '' : ' unproducible-card') + '">' +
         '<div class="image-card" style="background-image: url(' + card.url + ');"></div>' +
         '<div class="image-frame" style="background-image: url(' + card.frame + ');"></div>' +
         '<div class="text-stat" id="card-size"' + (card.category === '캐릭터' ? ' style="display:none;"' : '')  + '>' + card.size + '</div>' +
         '<div class="text-stat" id="card-atk"' + (card.category === '추종자' ? '' : ' style="display:none;"') + '>' + card.atk + '</div>' +
         '<div class="text-stat" id="card-def"' + (card.category === '추종자' ? '' : ' style="display:none;"') + '>' + card.defs + '</div>' +
         '<div class="text-stat" id="card-hp"' + (card.category === '스펠' ? ' style="display:none;"' : '') + '>' + card.hp + '</div>' +
+    '</div>' +
+    '<div class="' + className + '-title">' +
+        '<div class="top-row">' +
+            '<p class="p-name">' + card.name + '</p>' +
+            '<div class="right-column">' +
+                '<p class="p-episode">' + card.episode + '</p>' +
+                '<p class="p-rarity">' + card.rarity + '</p>' +
+            '</div>' +
+        '</div>' +
+        '<p class="p-tag">' + card.tag + '</p>' +
+    '</div>' + 
+    '<div class="' + className + '-skill">' +
+        '<p class="p-skill">' + card.skill_turn + '</p>' +
+        '<p class="p-skill">' + card.skill_instance + '</p>' +
+        '<p class="p-skill">' + card.skill_attack + '</p>' +
+        '<p class="p-skill">' + card.skill_defend + '</p>' +
+    '</div>' + 
+    '<div class="' + className + '-story" style="display:none;">' +
+        '<p class="p-skill">' + card.desc + '</p>' +
     '</div>'
 
     return result
 }
 
-function selectCard(id){
+function selectCard(id, type){
     $.ajax({
         url: '/card/select/' + id,
         type: 'GET',
@@ -24,34 +43,28 @@ function selectCard(id){
             }
             document.getElementById('capture-btn').style.display = 'inline-block';
             document.getElementById('swap-text-btn').style.display = 'inline-block';
+            if (type === 0) {
+                select_card_id = card.id;
+                document.getElementById('card-link-btn').style.display = 'inline-block';
+                if (card.link === "-1") {
+                    document.getElementById('card-link-btn').classList.add('grayscale');
+                }else {
+                    document.getElementById('card-link-btn').classList.remove('grayscale');
+                }
+                document.getElementById('card-link-prev').style.display = 'none';
+                document.getElementById('card-link-reset').style.display = 'none';
+                document.getElementById('card-link-next').style.display = 'none';
+                link_card_ids = card.link;
+            }
             $('.selected-card').html(
-                drawCard(card, "selected-card-image") + 
-                '<div class="selected-card-title">' +
-                    '<div class="top-row">' +
-                        '<p class="p-name">' + card.name + '</p>' +
-                        '<div class="right-column">' +
-                            '<p class="p-episode">' + card.episode + '</p>' +
-                            '<p class="p-rarity">' + card.rarity + '</p>' +
-                        '</div>' +
-                    '</div>' +
-                    '<p class="p-tag">' + card.tag + '</p>' +
-                '</div>' +
-                '<div class="selected-card-skill">' +
-                    '<p class="p-skill">' + card.skill_turn + '</p>' +
-                    '<p class="p-skill">' + card.skill_instance + '</p>' +
-                    '<p class="p-skill">' + card.skill_attack + '</p>' +
-                    '<p class="p-skill">' + card.skill_defend + '</p>' +
-                '</div>' + 
-                '<div class="selected-card-story" style="display:none;">' +
-                    '<p class="p-skill">' + card.desc + '</p>' +
-                '</div>'
+                drawCard(card, "selected-card")
             );
         }
     });
 }
 
 function swapTextTo(mode) {
-    var img = document.getElementById('swap-text-btn')
+    var img = document.getElementById('swap-text-btn');
 
     if (mode === 'story') {
         document.getElementsByClassName('selected-card-skill')[0].style.display = 'none';
@@ -74,4 +87,36 @@ function captureCard() {
         link.download = $('.selected-card-title .p-name').text();
         link.click(); // 링크 클릭
     });
+}
+
+function showLinkCard(offset) {
+    console.log(link_card_ids, link_card_idx, offset);
+    // no link card exist
+    if (link_card_ids === '-1') { 
+        return
+    }
+    // this is first link card, so previous does not exist
+    if (link_card_idx === 0 && offset === -1) { 
+        return
+    }
+    let cardIds = link_card_ids.split(',')
+    // this is last link card, so next does not exist
+    if (link_card_idx + offset === cardIds.length ) { 
+        return
+    }
+    link_card_idx += offset;
+    let cardId = parseInt(cardIds[link_card_idx]);
+    document.getElementById('card-link-prev').style.opacity = link_card_idx === 0 ? 0.5 : 1.0;
+    document.getElementById('card-link-next').style.opacity = link_card_idx + 1 < cardIds.length ? 1.0 : 0.5;
+    selectCard(cardId, 1);
+
+    document.getElementById('card-link-btn').style.display = 'none';
+    document.getElementById('card-link-prev').style.display = 'inline-block';
+    document.getElementById('card-link-reset').style.display = 'inline-block';
+    document.getElementById('card-link-next').style.display = 'inline-block';
+}
+
+function resetLinkCard() {
+    selectCard(select_card_id, 0);
+    link_card_idx = 0;
 }
