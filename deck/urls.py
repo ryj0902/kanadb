f9bc0abc7b9b37ae6caccebb4c8569906bb55485
ryj0902 <ryj0902@naver.com>
2025-01-23 10:59:59 +0900

    * remove temporary url in deck

--- deck/urls.py
+++ deck/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("<int:deck_id>", views.deck_detail, name="deck_detail"),
+    # path("temp", views.deck_temp, name="deck_temp"),
 ]
