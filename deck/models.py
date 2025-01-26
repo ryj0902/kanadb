12950e50276b5dc0181bb124bbcf392b3de93103
ryj0902 <ryj0902@naver.com>
2025-01-26 11:36:28 +0900

    * add fight npc deck

--- deck/models.py
+++ deck/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Deck(models.Model):
     id = models.IntegerField(primary_key=True)
-    chapter_name = models.CharField(max_length=40)
-    chapter_name_us = models.CharField(max_length=40)
-    loading_name = models.CharField(max_length=40)
-    loading_name_us = models.CharField(max_length=40)
+    chapter_name = models.TextField()
+    chapter_name_us = models.TextField()
+    loading_name = models.TextField()
+    loading_name_us = models.TextField()
     loading_desc = models.TextField()
     loading_desc_us = models.TextField()
     setup = models.TextField()
