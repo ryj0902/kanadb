f4ff0af1387872410b55f66041b321a7ce1f1f9d
ryj0902 <ryj0902@naver.com>
2025-01-26 12:53:47 +0900

    * add deck floor navigation

--- deck/views.py
+++ deck/views.py
@@ -9,7 +9,6 @@ from .models import Deck
 deck_total = Deck.objects.all()
 
 
-# Create your views here.
 def index(request):
     decks = Deck.objects.all()
 
@@ -19,7 +18,6 @@ def index(request):
 
 
 def deck_detail(request, deck_id):
-    # deck = Deck.objects.all().filter(id=deck_id)[0]
     deck = deck_total.filter(id=deck_id)[0]
     setup = list(map(lambda x: int(x), deck.setup.split(",")))
 
@@ -38,6 +36,14 @@ def deck_detail(request, deck_id):
         Card.parse_static_url(card)
         Card.parse_to_string(card)
 
+    # floor navigation
+    deck_floors = deck_total.filter(
+        id__gte=deck_id // 100 * 100, id__lt=(deck_id // 100 + 1) * 100
+    )
+    floors = list(deck_floors.values_list("id", flat=True))
+    floor_down = deck_id - 1 if deck_id - 1 in floors else -1
+    floor_up = deck_id + 1 if deck_id + 1 in floors else -1
+
     context = {
         "chapter_name": deck.chapter_name,
         "chapter_name_us": deck.chapter_name_us,
@@ -49,6 +55,14 @@ def deck_detail(request, deck_id):
         "card_list": sorted_cards,
         "horizontal_layout": _("가로 모드"),
         "vertical_layout": _("세로 모드"),
+        "floors": floors,
+        "current_floor": deck_id,
+        "floor_down": floor_down,
+        "floor_up": floor_up,
+    }
+
+    return render(request, "deck/deck_detail.html", context)
+
     }
 
     return render(request, "deck/deck_detail.html", context)
