83a2e0022493b28576a6eb1e2d3b4c7a2761137b
ryj0902 <ryj0902@naver.com>
2025-08-17 23:06:17 +0900

    * add new nether dungeon: Cobalt Memory * add fixed-term exam hall: Tower of Summer

--- image_process.py
+++ image_process.py
@@ -7,36 +7,32 @@ from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_compl
 from tqdm import tqdm
 
 
-def hash_png_file(rel_path, path):
-    """단일 PNG 파일에 대한 해시 계산"""
+def hash_png_file(path, base_dir):
+    """파일 해시 값 계산"""
     with open(path, "rb") as f:
         file_hash = hashlib.md5(f.read()).hexdigest()
+    rel_path = os.path.relpath(path, base_dir)
     return rel_path, file_hash
 
 
-def get_png_file_list(directory):
-    """디렉토리 내 PNG 파일들의 절대 경로 목록 반환"""
+def get_file_list(directory, extension=".png"):
+    """디렉토리 내 특정 확장자 파일들의 절대 경로 목록 반환"""
     file_list = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.lower().endswith(".png"):
-                path = os.path.join(root, file)
-                file_list.append(path)
+    for file in os.listdir(directory):
+        if file.lower().endswith(extension):
+            path = os.path.join(directory, file)
+            file_list.append(path)
     return file_list
 
 
 def get_png_hashes(directory, max_workers=8):
-    """디렉토리 내 모든 PNG 파일의 해시값을 반환"""
+    """디렉토리 내 모든 png 파일에 대한 해시 값 반환"""
     hashes = {}
+    png_files = get_file_list(directory, ".png")
 
-    # PNG 파일 목록 수집
-    png_files = get_png_file_list(directory)
-
-    # 멀티스레딩 해싱
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = {
-            executor.submit(hash_png_file, rel_path, path): rel_path
-            for rel_path, path in png_files
+            executor.submit(hash_png_file, path, directory): path for path in png_files
         }
         for future in tqdm(
             as_completed(futures), total=len(futures), desc=f"Hashing: {directory}"
@@ -52,15 +48,21 @@ def copy_changed_files(target_dir, orig_dir, old_dir, new_dir):
     os.makedirs(new_dir, exist_ok=True)
     target_hashes = get_png_hashes(target_dir)
     orig_hashes = get_png_hashes(orig_dir)
-    old_files = get_png_file_list(old_dir)
+    old_files = get_file_list(old_dir, ".webp")
 
+    # 복사가 필요한 파일 리스트 추출
+    copy_list = []
     for rel_path, target_hash in target_hashes.items():
         orig_hash = orig_hashes.get(rel_path)
-        if orig_hash != target_hash or rel_path not in old_files:
-            src_path = os.path.join(target_dir, rel_path)
-            dest_path = os.path.join(new_dir, rel_path)
-            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-            shutil.copy2(src_path, dest_path)
+        filename = os.path.join(old_dir, rel_path.replace(".png", ".webp"))
+        if orig_hash != target_hash or filename not in old_files:
+            copy_list.append(rel_path)
+    print(f"[INFO] 신규 파일 개수: {len(copy_list)}")
+
+    for rel_path in tqdm(copy_list, desc="Copy to new dir"):
+        src_path = os.path.join(target_dir, rel_path)
+        dest_path = os.path.join(new_dir, rel_path)
+        shutil.copy2(src_path, dest_path)
 
 
 def convert_single_png_to_webp(png_path):
@@ -79,7 +81,7 @@ def convert_single_png_to_webp(png_path):
 
 def convert_png_to_webp(directory, max_workers=8):
     """PNG → WebP 변환 작업을 멀티프로세싱으로 수행"""
-    png_files = get_png_file_list(directory)
+    png_files = get_file_list(directory, ".png")
 
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         futures = {
@@ -98,7 +100,7 @@ def convert_png_to_webp(directory, max_workers=8):
 
 def delete_png_files(directory):
     """디렉토리 내 PNG 파일 삭제"""
-    png_files = get_png_file_list(directory)
+    png_files = get_file_list(directory, ".png")
 
     for file in tqdm(png_files, desc="Deleting png"):
         os.remove(file)
@@ -110,29 +112,34 @@ def merge_directories(src_dir, dst_dir):
         for file in files:
             rel_path = os.path.relpath(os.path.join(root, file), src_dir)
             dst_path = os.path.join(dst_dir, rel_path)
-            os.makedirs(os.path.dirname(dst_path), exist_ok=True)
             shutil.copy2(os.path.join(root, file), dst_path)
 
 
 if __name__ == "__main__":
+    # 방금 추출한 png 파일들
     target_dir = "D:\workspace\Kanatales\kanadb\static\card\Texture2D"
+    # 기존에 추출했었던 png 파일들
     orig_dir = "D:\workspace\Kanatales\kanadb\static\card\Texture2D_orig"
-    new_dir = "D:\workspace\Kanatales\kanadb\static\card\Texture2D_new"
+    # 기존에 사용하던 webp 파일들
     old_dir = "D:\workspace\Kanatales\kanadb\static\card\Texture2D_old"
+    # 새로 갱신해야 할 webp 파일들
+    new_dir = "D:\workspace\Kanatales\kanadb\static\card\Texture2D_new"
 
-    # # 1. 변경 파일 복사
-    # copy_changed_files(target_dir, orig_dir, old_dir, new_dir)
-    # # 2. PNG → WebP 변환
-    # convert_png_to_webp(new_dir)
-    # # 3. PNG 삭제
-    # delete_png_files(new_dir)
-    # # 4. orig_dir 제거 + target_dir → orig_dir 이름 변경
-    # shutil.rmtree(orig_dir)
-    # os.rename(target_dir, orig_dir)
-    # # 5. new_dir → old_dir 병합, old_dir → target_dir 변경
-    # merge_directories(new_dir, old_dir)
-    # os.rename(old_dir, target_dir)
+    # 1. 변경 파일 복사
+    copy_changed_files(target_dir, orig_dir, old_dir, new_dir)
+    # 2. PNG → WebP 변환
+    convert_png_to_webp(new_dir)
+    # 3. PNG 삭제
+    delete_png_files(new_dir)
+    # 4. orig_dir 제거 + target_dir → orig_dir 이름 변경
+    print(f"Remove {orig_dir}")
+    shutil.rmtree(orig_dir)
+    os.rename(target_dir, orig_dir)
+    # 5. new_dir → old_dir 병합, old_dir → target_dir 변경
+    print("Merge new → old dir")
+    merge_directories(new_dir, old_dir)
+    print("Rename Texture2D_old → Texture2D")
+    os.rename(old_dir, target_dir)
 
-    convert_png_to_webp(target_dir)
-
-    delete_png_files(target_dir)
+    # convert_png_to_webp(target_dir)
+    # delete_png_files(target_dir)
